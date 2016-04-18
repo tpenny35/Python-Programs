ef9a95ec9a2048137c59febe066558de854c993c
tpenny35 <tpenny22@gmail.com>
2016-04-17 19:09:20 -0500

    Email module
    upgrade to my original attempt

--- pymailer2.py
+++ pymailer2.py
@@ -6,8 +6,8 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
-mailName = "pymail1017@gmail.com"
-mailPass = "P3nny1017"
+mailName = "Email"
+mailPass = "Password"
 
 def sendmail(sendFrom,sendTo,sendSub,sendMsg,attachment):
     sendSub = sendSub + "\n"
